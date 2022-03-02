e68e736e844ad36d84068016536db21cae026478
Mabelwang233 <66892139+Mabelwang233@users.noreply.github.com>
2022-03-03 07:22:29 +0800

    Add move3

--- movetest.py
+++ movetest.py
@@ -1,1 +1,19 @@
-
+while move3:
+  resetto90()
+  for angle in range(102, 2, -4):# turn right leg
+        leg_right.angle =  angle
+        time.sleep(0.05)
+  for angle in range(90, 178, 4):# turn left leg
+        leg_left.angle =  angle
+        time.sleep(0.05)
+        
+  for angle in range(90, 150, 10):
+        foot_left.angle = angle
+        foot_right.angle = 180-angle
+        time.sleep(0.5)
+  for angle in range(150, 90, -10):
+        foot_left.angle = angle
+        foot_right.angle = 180-angle
+        time.sleep(0.5)
+        
+  resetto90()
