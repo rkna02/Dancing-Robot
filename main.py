b828132cb22ad39e97853f37b75b8ca7c5b71e7f
Mabelwang233 <66892139+Mabelwang233@users.noreply.github.com>
2022-03-07 11:43:43 -0800

    add some music

--- main.py
+++ main.py
@@ -37,7 +37,134 @@ music = False
 
 count = 0
 
+#muisc
+
+# Define pin connected to piezo buzzer.
+PIEZO_PIN = board.A1
+#Pachelbel's Canon
+
+NOTE_B0=31
+NOTE_C1=33
+NOTE_CS1=35
+NOTE_D1=37
+NOTE_DS1=39
+NOTE_E1=41
+NOTE_F1=44
+NOTE_FS1=46
+NOTE_G1=49
+NOTE_GS1=52
+NOTE_A1=55
+NOTE_AS1=58
+NOTE_B1=62
+NOTE_C2=65
+NOTE_CS2=69
+NOTE_D2=73
+NOTE_DS2=78
+NOTE_E2=82
+NOTE_F2=87
+NOTE_FS2=93
+NOTE_G2=98
+NOTE_GS2=104
+NOTE_A2=110
+NOTE_AS2=117
+NOTE_B2=123
+NOTE_C3=131
+NOTE_CS3=139
+NOTE_D3=147
+NOTE_DS3=156
+NOTE_E3=165
+NOTE_F3=175
+NOTE_FS3=185
+NOTE_G3=196
+NOTE_GS3=208
+NOTE_A3=220
+NOTE_AS3=233
+NOTE_B3=247
+NOTE_C4=262
+NOTE_CS4=277
+NOTE_D4=294
+NOTE_DS4=311
+NOTE_E4=330
+NOTE_F4=349
+NOTE_FS4=370
+NOTE_G4=392
+NOTE_GS4=415
+NOTE_A4=440
+NOTE_AS4=466
+NOTE_B4=494
+NOTE_C5=523
+NOTE_CS5=554
+NOTE_D5=587
+NOTE_DS5=622
+NOTE_E5=659
+NOTE_F5=698
+NOTE_FS5=740
+NOTE_G5=784
+NOTE_GS5=831
+NOTE_A5=880
+NOTE_AS5=932
+NOTE_B5=988
+NOTE_C6=1047
+NOTE_CS6=1109
+NOTE_D6=1175
+NOTE_DS6=1245
+NOTE_E6=1319
+NOTE_F6=1397
+NOTE_FS6=1480
+NOTE_G6=1568
+NOTE_GS6=1661
+NOTE_A6=1760
+NOTE_AS6=1865
+NOTE_B6=1976
+NOTE_C7=2093
+NOTE_CS7=2217
+NOTE_D7=2349
+NOTE_DS7=2489
+NOTE_E7=2637
+NOTE_F7=2794
+NOTE_FS7=2960
+NOTE_G7=3136
+NOTE_GS7=3322
+NOTE_A7=3520
+NOTE_AS7=3729
+NOTE_B7=3951
+NOTE_C8=4186
+NOTE_CS8=4435
+NOTE_D8=4699
+NOTE_DS8=4978
+REST=0
+
+
+
+# change this to make the song slower or faster
+tempo = 2
+
+
+melody1 = [NOTE_C5,4,
+  NOTE_F5,4, NOTE_F5,8, NOTE_G5,8, NOTE_F5,8, NOTE_E5,8,
+  NOTE_D5,4, NOTE_D5,4, NOTE_D5,4,
+  NOTE_G5,4, NOTE_G5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
+  NOTE_E5,4, NOTE_C5,4, NOTE_C5,4,
+  NOTE_A5,4, NOTE_A5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8,
+  NOTE_F5,4, NOTE_D5,4, NOTE_C5,8, NOTE_C5,8,
+  NOTE_D5,4, NOTE_G5,4, NOTE_E5,4,
+  NOTE_F5,2
+] #12s
+melody2 = [NOTE_C5,4,
+  NOTE_F5,4, NOTE_F5,4, NOTE_F5,4,
+  NOTE_E5,2, NOTE_E5,4,
+  NOTE_F5,4, NOTE_E5,4, NOTE_D5,4,
+  NOTE_C5,2, NOTE_A5,4,
+  NOTE_AS5,4, NOTE_A5,4, NOTE_G5,4,
+  NOTE_C6,4, NOTE_C5,4, NOTE_C5,8, NOTE_C5,8,
+  NOTE_D5,4, NOTE_G5,4, NOTE_E5,4,
+  NOTE_F5,2
+]
+
+
+i = 0
 while move1:
+    simpleio.tone(PIEZO_PIN, melody1[i], tempo/melody1[i+1])
     for angle in range(80, 100, 5):
         leg_left.angle = angle
         leg_right.angle = angle
@@ -46,16 +173,30 @@ while move1:
         leg_left.angle = angle
         leg_right.angle =  angle
         time.sleep(0.05)
-
+    i = i+2    
+    simpleio.tone(PIEZO_PIN, melody1[i], tempo/melody1[i+1])
     for angle in range(90, 150, 10):
         foot_left.angle = angle
         foot_right.angle = 180-angle
         time.sleep(0.1)
+    i = i+2
+    simpleio.tone(PIEZO_PIN, melody1[i], tempo/melody1[i+1]) 
+    i = i+2
+    simpleio.tone(PIEZO_PIN, melody1[i], tempo/melody1[i+1]) 
+    i = i+2
+    simpleio.tone(PIEZO_PIN, melody1[i], tempo/melody1[i+1]) 
+    i = i+2
+    simpleio.tone(PIEZO_PIN, melody1[i], tempo/melody1[i+1])
+
     for angle in range(150, 90, -10):
         foot_left.angle = angle
         foot_right.angle = 180-angle
         time.sleep(0.1)
     resetto90()
+    i = i+2
+    simpleio.tone(PIEZO_PIN, melody1[i], tempo/melody1[i+1]) 
+    i = i+2
+    simpleio.tone(PIEZO_PIN, melody1[i], tempo/melody1[i+1]) 
     time.sleep(0.05)
     count=count+1
     if count == 3:
@@ -245,130 +386,7 @@ while move6:
         leg_left.angle = leg_left.angle + 4
         foot_left.angle = foot_left.angle -2
 
-#muisc
 
-# Define pin connected to piezo buzzer.
-PIEZO_PIN = board.A1
-PIEZO_PIN = board.A1
-
-#Pachelbel's Canon
-
-NOTE_B0=31
-NOTE_C1=33
-NOTE_CS1=35
-NOTE_D1=37
-NOTE_DS1=39
-NOTE_E1=41
-NOTE_F1=44
-NOTE_FS1=46
-NOTE_G1=49
-NOTE_GS1=52
-NOTE_A1=55
-NOTE_AS1=58
-NOTE_B1=62
-NOTE_C2=65
-NOTE_CS2=69
-NOTE_D2=73
-NOTE_DS2=78
-NOTE_E2=82
-NOTE_F2=87
-NOTE_FS2=93
-NOTE_G2=98
-NOTE_GS2=104
-NOTE_A2=110
-NOTE_AS2=117
-NOTE_B2=123
-NOTE_C3=131
-NOTE_CS3=139
-NOTE_D3=147
-NOTE_DS3=156
-NOTE_E3=165
-NOTE_F3=175
-NOTE_FS3=185
-NOTE_G3=196
-NOTE_GS3=208
-NOTE_A3=220
-NOTE_AS3=233
-NOTE_B3=247
-NOTE_C4=262
-NOTE_CS4=277
-NOTE_D4=294
-NOTE_DS4=311
-NOTE_E4=330
-NOTE_F4=349
-NOTE_FS4=370
-NOTE_G4=392
-NOTE_GS4=415
-NOTE_A4=440
-NOTE_AS4=466
-NOTE_B4=494
-NOTE_C5=523
-NOTE_CS5=554
-NOTE_D5=587
-NOTE_DS5=622
-NOTE_E5=659
-NOTE_F5=698
-NOTE_FS5=740
-NOTE_G5=784
-NOTE_GS5=831
-NOTE_A5=880
-NOTE_AS5=932
-NOTE_B5=988
-NOTE_C6=1047
-NOTE_CS6=1109
-NOTE_D6=1175
-NOTE_DS6=1245
-NOTE_E6=1319
-NOTE_F6=1397
-NOTE_FS6=1480
-NOTE_G6=1568
-NOTE_GS6=1661
-NOTE_A6=1760
-NOTE_AS6=1865
-NOTE_B6=1976
-NOTE_C7=2093
-NOTE_CS7=2217
-NOTE_D7=2349
-NOTE_DS7=2489
-NOTE_E7=2637
-NOTE_F7=2794
-NOTE_FS7=2960
-NOTE_G7=3136
-NOTE_GS7=3322
-NOTE_A7=3520
-NOTE_AS7=3729
-NOTE_B7=3951
-NOTE_C8=4186
-NOTE_CS8=4435
-NOTE_D8=4699
-NOTE_DS8=4978
-REST=0
-
-
-
-# change this to make the song slower or faster
-tempo = 200
-
-
-melody = [NOTE_C5,4,
-  NOTE_F5,4, NOTE_F5,8, NOTE_G5,8, NOTE_F5,8, NOTE_E5,8,
-  NOTE_D5,4, NOTE_D5,4, NOTE_D5,4,
-  NOTE_G5,4, NOTE_G5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
-  NOTE_E5,4, NOTE_C5,4, NOTE_C5,4,
-  NOTE_A5,4, NOTE_A5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8,
-  NOTE_F5,4, NOTE_D5,4, NOTE_C5,8, NOTE_C5,8,
-  NOTE_D5,4, NOTE_G5,4, NOTE_E5,4,
-
-  NOTE_F5,2, NOTE_C5,4,
-  NOTE_F5,4, NOTE_F5,8, NOTE_G5,8, NOTE_F5,8, NOTE_E5,8,
-  NOTE_D5,4, NOTE_D5,4, NOTE_D5,4,
-  NOTE_G5,4, NOTE_G5,8, NOTE_A5,8, NOTE_G5,8, NOTE_F5,8,
-  NOTE_E5,4, NOTE_C5,4, NOTE_C5,4,
-  NOTE_A5,4, NOTE_A5,8, NOTE_AS5,8, NOTE_A5,8, NOTE_G5,8,
-  NOTE_F5,4, NOTE_D5,4, NOTE_C5,8, NOTE_C5,8,
-  NOTE_D5,4, NOTE_G5,4, NOTE_E5,4,
-  NOTE_F5,2, NOTE_C5,4
-]
 
 #sizeof gives the number of bytes, each int value is composed of two bytes (16 bits)
 #there are two values per note (pitch and duration), so for each note there are four bytes
@@ -379,20 +397,4 @@ wholenote = (60* 4) / tempo
 
 divider = 0
 noteDuration = 0
-while music:
-    time.sleep(1000)
 
-    # iterate over the notes of the melody.
-    # Remember, the array is twice the number of notes (notes + durations)
-    for i in range (0,notes*2,2):
-
-        # calculates the duration of each note
-        divider = melody[i + 1]
-          # regular note, just proceed
-        noteDuration = (wholenote) / divider
-
-        #we only play the note for 90% of the duration, leaving 10% as a pause
-        simpleio.tone(PIEZO_PIN, melody[i], duration = noteDuration)
-
-         #Wait for the specief duration before playing the next note.
-        time.sleep(noteDuration)
